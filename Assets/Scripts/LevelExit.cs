9bc8586d18854cff36e22233fc39ba82b9fcdbab
se468 <se468@hotmail.com>
2020-01-05 21:16:21 -0500

    level exit portal

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float LevelLoadDelay = 2f;
+    [SerializeField] float LevelExitSlowMoFactor = 0.2f;
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger Enter");
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = LevelExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        Time.timeScale = 1f;
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
