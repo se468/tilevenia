25781ff3ac03449e56e2f20ddaf5b6c53e602d5a
se468 <se468@hotmail.com>
2020-01-19 13:06:28 -0500

    added background music and spring

---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    [SerializeField] float springSpeed = 100f;
+    private void OnCollisionEnter2D(Collision2D col) {
+        Debug.Log("SPRING!");
+
+        if (LayerMask.LayerToName(col.gameObject.layer) == "Player" &&
+            col.collider.GetType() == typeof(UnityEngine.BoxCollider2D)) {
+            Debug.Log("SPRING!");
+            Rigidbody2D playerBody = col.gameObject.GetComponent<Rigidbody2D>();
+            Vector2 playerVelocity = new Vector2(playerBody.velocity.x, springSpeed);
+            playerBody.velocity = playerVelocity;
+        }
+    }
+}
